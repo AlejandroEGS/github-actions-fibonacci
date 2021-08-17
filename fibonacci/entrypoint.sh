16e70709cd6773f27c488523458f53fff5ffda95
Alejandro Gonzalez <sagbinie@uninorte.edu.co>
2021-08-17 10:48:25 -0500

    fix: Change entrypoint.sh

--- fibonacci/entrypoint.sh
+++ fibonacci/entrypoint.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-def fibo(num):
-    lista = list()
-    lista.append(1)
-    lista.append(1)
-    f = 0
-    for i in range(num - 2):
-        f = lista[i] + lista[i + 1]
-        lista.append(f)
-
-    print('Secuencia: ', lista)
-    return lista[num - 1]
-
-num = $INPUT_N_INDEX
-print("El número es: ", fibo(num))
+python fibonacci.py %INPUT_N_INDEX
