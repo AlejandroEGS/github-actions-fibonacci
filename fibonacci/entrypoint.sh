57a844d5a64658ddd9431ca87afb97f0b195fb48
Alejandro Gonzalez <sagbinie@uninorte.edu.co>
2021-08-17 16:22:58 -0500

    fix: Dockerfile, fibonacci.py, entrypoint.sh

--- fibonacci/entrypoint.sh
+++ fibonacci/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python fibonacci.py %INPUT_N_INDEX
+python fibonacci.py INPUT_N_INDEX
--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,13 +1,21 @@
-def fibo(num):
-    lista = list()
-    lista.append(1)
-    lista.append(1)
-    f = 0
-    for i in range(num - 2):
-        f = lista[i] + lista[i + 1]
-        lista.append(f)
+import sys
 
-    print('Secuencia: ', lista)
-    print("El número es: ", fibo(num))
-
-num = int(input())
+#  Function for nth Fibonacci number
+ 
+def Fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==0:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==1:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+ 
+# Driver Program
+ 
+print(Fibonacci(int(sys.argv[1])))
+ 
+#This code is contributed by Saket Modi
